12fe810abb0a471c868f816b778ea065982a1746
faheemw09 <faheemh712@gmail.com>
2024-10-28 12:24:02 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -17,26 +17,28 @@ function App() {
           path="/"
           element={
             <>
-              <div className="flex flex-col items-center justify-center flex-grow mt-20">
-                <div className="image-container">
-                  <img
-                    src="/images/image1.png"
-                    alt="App Banner"
-                    className="w-60 h-70"
-                  />
+              <div className="main-content">
+                <div className="flex flex-col items-center justify-center flex-grow mt-20">
+                  <div className="image-container">
+                    <img
+                      src="/images/image1.png"
+                      alt="App Banner"
+                      className="w-60 h-70"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col items-center justify-center flex-grow mt-10">
-                <h4 className="text-black font-bold text-[20px] leading-none">
-                  Goftego
-                </h4>
-                <h6 className="text-black mt-2 text-center leading-none">
-                  Welcome to Your Friendly <br /> Chat Companion!
-                </h6>
-              </div>
+                <div className="flex flex-col items-center justify-center flex-grow mt-10">
+                  <h4 className="text-black font-bold text-[20px] leading-none">
+                    Goftego
+                  </h4>
+                  <h6 className="text-black mt-2 text-center leading-none">
+                    Welcome to Your Friendly <br /> Chat Companion!
+                  </h6>
+                </div>
 
-              <div className="mt-6 mb-3 flex justify-center">
-                <MainButton text={"Get Started"} onClick={handleClick} />
+                <div className="mt-6 mb-3 flex justify-center">
+                  <MainButton text={"Get Started"} onClick={handleClick} />
+                </div>
               </div>
             </>
           }
--- client/src/component/authSection/signup.js
+++ client/src/component/authSection/signup.js
@@ -88,142 +88,144 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      {/* {isloading && <SpinnerComponent />} */}
-      <div className="flex flex-col bg-primary bor h-[150px] w-full p-4 pt-[30px] rounded-2xl">
-        <img
-          src="/images/arrow.png"
-          alt="Back"
-          height={20}
-          width={20}
-          onClick={() => navigate("/")}
-          className="cursor-pointer mb-2 pt-1"
-        />
-        <h1 className="text-white pt-3 text-left text-[28px] font-semibold leading-none ">
-          Register
-        </h1>
-        <h5 className="text-white  text-left">
-          Fill up your details to register.
-        </h5>
-      </div>
-
-      <form
-        onSubmit={handleSignup}
-        className="flex flex-col items-center justify-center space-y-1 w-full mt-4 rounded-lg"
-      >
-        <div className="flex flex-col mb-1">
-          <label htmlFor="name" className="text-black mb-1 text-[14px]">
-            Name <span className="text-red-500">*</span>
-          </label>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="input-field"
-            required
+    <div className="main-content">
+      <div className="flex flex-col items-center justify-center">
+        {/* {isloading && <SpinnerComponent />} */}
+        <div className="flex flex-col bg-primary bor h-[150px] w-full p-4 pt-[30px] rounded-2xl">
+          <img
+            src="/images/arrow.png"
+            alt="Back"
+            height={20}
+            width={20}
+            onClick={() => navigate("/")}
+            className="cursor-pointer mb-2 pt-1"
           />
-          {nameError && <p className="text-red-500 text-xs">{nameError}</p>}
+          <h1 className="text-white pt-3 text-left text-[28px] font-semibold leading-none ">
+            Register
+          </h1>
+          <h5 className="text-white  text-left">
+            Fill up your details to register.
+          </h5>
         </div>
 
-        <div className="flex flex-col mb-2">
-          <label htmlFor="email" className="text-black mb-1 text-[14px]">
-            Email <span className="text-red-500">*</span>
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="input-field"
-            required
-          />
-          {emailError && <p className="text-red-500 text-xs">{emailError}</p>}
-        </div>
-
-        <div className="flex flex-col mb-2 relative">
-          <label htmlFor="password" className="text-black mb-1 text-[14px]">
-            Password <span className="text-red-500">*</span>
-          </label>
-          <div className="relative w-full">
+        <form
+          onSubmit={handleSignup}
+          className="flex flex-col items-center justify-center space-y-1 w-full mt-4 rounded-lg"
+        >
+          <div className="flex flex-col mb-1">
+            <label htmlFor="name" className="text-black mb-1 text-[14px]">
+              Name <span className="text-red-500">*</span>
+            </label>
             <input
-              id="password"
-              name="password"
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="input-field pr-10"
+              id="name"
+              name="name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="input-field"
               required
             />
-            <span
-              onClick={togglePasswordVisibility}
-              className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
-            >
-              {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-            </span>
+            {nameError && <p className="text-red-500 text-xs">{nameError}</p>}
           </div>
-          {passwordError && (
-            <p className="text-red-500 text-xs">{passwordError}</p>
+
+          <div className="flex flex-col mb-2">
+            <label htmlFor="email" className="text-black mb-1 text-[14px]">
+              Email <span className="text-red-500">*</span>
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input-field"
+              required
+            />
+            {emailError && <p className="text-red-500 text-xs">{emailError}</p>}
+          </div>
+
+          <div className="flex flex-col mb-2 relative">
+            <label htmlFor="password" className="text-black mb-1 text-[14px]">
+              Password <span className="text-red-500">*</span>
+            </label>
+            <div className="relative w-full">
+              <input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input-field pr-10"
+                required
+              />
+              <span
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+              >
+                {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+              </span>
+            </div>
+            {passwordError && (
+              <p className="text-red-500 text-xs">{passwordError}</p>
+            )}
+          </div>
+
+          <div className="flex flex-col mb-[20px]">
+            <label className="text-black mb-1 text-[14px]">
+              Gender <span className="text-red-500">*</span>
+            </label>
+            <div className="flex space-x-2">
+              <label className="flex items-center text-[14px]">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  onChange={(e) => setGender(e.target.value)}
+                  required
+                />
+                Male
+              </label>
+              <label className="flex items-center text-[14px]">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  onChange={(e) => setGender(e.target.value)}
+                  required
+                />
+                Female
+              </label>
+            </div>
+          </div>
+
+          {/* Display Backend Error in UI */}
+          {genderError && (
+            <div className="text-red-500 text-sm mb-2">{genderError}</div>
           )}
-        </div>
 
-        <div className="flex flex-col mb-[20px]">
-          <label className="text-black mb-1 text-[14px]">
-            Gender <span className="text-red-500">*</span>
-          </label>
-          <div className="flex space-x-2">
-            <label className="flex items-center text-[14px]">
-              <input
-                type="radio"
-                name="gender"
-                value="male"
-                onChange={(e) => setGender(e.target.value)}
-                required
-              />
-              Male
-            </label>
-            <label className="flex items-center text-[14px]">
-              <input
-                type="radio"
-                name="gender"
-                value="female"
-                onChange={(e) => setGender(e.target.value)}
-                required
-              />
-              Female
-            </label>
+          <div className="mt-[50px]">
+            <MainButton
+              type="submit"
+              disabled={loading}
+              loading={loading}
+              text={"Signup"}
+              onClick={handleSignup}
+            >
+              {loading ? "Registering..." : "Submit"}
+            </MainButton>
           </div>
+        </form>
+        <div>
+          <h5 className="text-black mt-4 text-[12px] leading-none">
+            already have an account ?{" "}
+            <span
+              onClick={() => navigate("/login")}
+              className="text-primary cursor-pointer font-semibold"
+            >
+              login
+            </span>
+          </h5>
         </div>
-
-        {/* Display Backend Error in UI */}
-        {genderError && (
-          <div className="text-red-500 text-sm mb-2">{genderError}</div>
-        )}
-
-        <div className="mt-[50px]">
-          <MainButton
-            type="submit"
-            disabled={loading}
-            loading={loading}
-            text={"Signup"}
-            onClick={handleSignup}
-          >
-            {loading ? "Registering..." : "Submit"}
-          </MainButton>
-        </div>
-      </form>
-      <div>
-        <h5 className="text-black mt-4 text-[12px] leading-none">
-          already have an account ?{" "}
-          <span
-            onClick={() => navigate("/login")}
-            className="text-primary cursor-pointer font-semibold"
-          >
-            login
-          </span>
-        </h5>
       </div>
     </div>
   );
